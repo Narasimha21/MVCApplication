444f3dcfba62eef16caa19c8360cd57e30a85672
Narasimha <nrjava8@gmail.com>
2017-09-23 11:29:54 -0500

    adding main class

---
+++ src/main/java/com/narasimha/mvc/SpringMVCApplication.java
@@ -0,0 +1,20 @@
+package com.narasimha.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringMVCApplication extends SpringBootServletInitializer {
+
+@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringMVCApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringMVCApplication.class, args);
+	}
+
+}
